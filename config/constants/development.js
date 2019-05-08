92c710f69bf7c545e8203270e4d2caf58599bad1
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-05-08 21:54:51 +0300

    Fix

--- config/constants/development.js
+++ config/constants/development.js
@@ -2,12 +2,12 @@ module.exports = {
   ACCOUNTS_APP_CONNECTOR_URL: 'https://accounts.topcoder-dev.com/connector.html',
   ACCOUNTS_APP_LOGIN_URL: 'https://accounts.topcoder-dev.com/member',
   COMMUNITY_APP_URL: 'https://www.topcoder-dev.com',
-  MEMBER_API_URL: 'http://api.topcoder-dev.com/v4/members',
-  MEMBER_API_V3_URL: 'http://api.topcoder-dev.com/v3/members',
+  MEMBER_API_URL: 'https://api.topcoder-dev.com/v4/members',
+  MEMBER_API_V3_URL: 'https://api.topcoder-dev.com/v3/members',
   ARENA_URL: 'https://arena.topcoder-dev.com',
-  DEV_APP_URL: 'http://local.topcoder-dev.com',
-  CHALLENGE_API_URL: 'http://api.topcoder-dev.com/v4',
-  PROJECT_API_URL: 'http://api.topcoder-dev.com/v3',
-  SUBMISSION_REVIEW_API_URL: 'http://local.topcoder-dev.com:3010',
+  DEV_APP_URL: 'https://local.topcoder-dev.com',
+  CHALLENGE_API_URL: 'https://api.topcoder-dev.com/v4',
+  PROJECT_API_URL: 'https://api.topcoder-dev.com/v3',
+  SUBMISSION_REVIEW_API_URL: 'https://local.topcoder-dev.com:3010',
   FORUM_URL: 'https://apps.topcoder-dev.com/forums/?module=Category&categoryID='
 }
