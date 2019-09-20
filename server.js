f34759f67ec661323a11ff5da4b952abe4ddd781
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-09-20 10:57:52 +0300

    Add health check endpoint

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 
 const requireHTTPS = (req, res, next) => {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
     return res.redirect('https://' + req.get('host') + req.url)
   }
   next()
@@ -15,10 +15,18 @@ app.use(requireHTTPS)
 
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
+
+// the topcoder-healthcheck-dropin library returns checksRun count,
+// here it follows that to return such count
+let checksRun = 0
+
+app.get('/health', (req, res) => {
+  checksRun += 1
+  res.json({ checksRun })
+})
+
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
 const port = process.env.PORT || 3000
 app.listen(port)
 
 console.log(`App is listening on port ${port}`)
-
-
