b73a3f941246d928e8d8ed6708d585c9149e7809
James Cori <jamescori@gmail.com>
2020-04-22 12:15:55 -0400

    Fixing the endpoints now that the API CORS issue is fixed.

--- config/constants/production.js
+++ config/constants/production.js
@@ -9,11 +9,14 @@ module.exports = {
   MEMBER_API_V3_URL: `${PROD_API_HOSTNAME}/v3/members`,
   DEV_APP_URL: 'https://submission-review.topcoder.com',
   CHALLENGE_API_URL: `${PROD_API_HOSTNAME}/v5/challenges`,
-  CHALLENGE_TIMELINE_TEMPLATES_URL: `${PROD_API_HOSTNAME}/v5/timelinetemplates`,
-  CHALLENGE_TYPES_URL: `${PROD_API_HOSTNAME}/v5/challengetypes`,
-  CHALLENGE_PHASES_URL: `${PROD_API_HOSTNAME}/v5/challengephases`,
+  CHALLENGE_TIMELINE_TEMPLATES_URL: `${PROD_API_HOSTNAME}/v5/timeline-templates`,
+  CHALLENGE_TYPES_URL: `${PROD_API_HOSTNAME}/v5/challenge-types`,
+  CHALLENGE_PHASES_URL: `${PROD_API_HOSTNAME}/v5/challenge-phases`,
   PROJECT_API_URL: `${PROD_API_HOSTNAME}/v5/projects`,
   GROUPS_API_URL: `${PROD_API_HOSTNAME}/v5/groups`,
-  PLATFORMS_V4_API_URL: `${DEV_API_HOSTNAME}/v4/platforms`,
-  TECHNOLOGIES_V4_API_URL: `${DEV_API_HOSTNAME}/v4/technologies`
+  TERMS_API_URL: `${PROD_API_HOSTNAME}/v5/terms`,
+  RESOURCES_API_URL: `${PROD_API_HOSTNAME}/v5/resources`,
+  RESOURCE_ROLES_API_URL: `${PROD_API_HOSTNAME}/v5/resource-roles`,
+  PLATFORMS_V4_API_URL: `${PROD_API_HOSTNAME}/v4/platforms`,
+  TECHNOLOGIES_V4_API_URL: `${PROD_API_HOSTNAME}/v4/technologies`
 }
