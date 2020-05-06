4a7bf2f77546799ec255d74e4660ed7263332f34
diazz <dat.nguyen.supper@gmail.com>
2020-05-06 16:06:25 +0700

    #239 Tweak auto-PATCH of specification fields
    #239 Tweak auto-PATCH of specification fields

--- src/components/ChallengeEditor/Description-Field/index.js
+++ src/components/ChallengeEditor/Description-Field/index.js
@@ -3,17 +3,36 @@ import styles from './Description-Field.module.scss'
 import PropTypes from 'prop-types'
 import SimpleMDE from 'simplemde'
 import cn from 'classnames'
+import _ from 'lodash'
 
 class DescriptionField extends Component {
   constructor (props) {
     super(props)
     this.ref = React.createRef()
+    this.state = {
+      isChanged: false
+    }
+    this.blurTheFieldThrottled = _.throttle(this.blurTheField.bind(this), 10000) // 10s
+  }
+
+  blurTheField () {
+    const { onBlurDescription, type } = this.props
+    onBlurDescription(this.simplemde.value(), type)
   }
 
   componentDidMount () {
     const { challenge, onUpdateDescription, type } = this.props
     this.simplemde = new SimpleMDE({ element: this.ref.current, initialValue: challenge[type] })
-    this.simplemde.codemirror.on('change', () => onUpdateDescription(this.simplemde.value(), type))
+    this.simplemde.codemirror.on('change', () => {
+      this.setState({ isChanged: true })
+      onUpdateDescription(this.simplemde.value(), type)
+    })
+    this.simplemde.codemirror.on('blur', () => {
+      if (this.state.isChanged) {
+        this.setState({ isChanged: false })
+        this.blurTheFieldThrottled()
+      }
+    })
   }
 
   render () {
@@ -27,12 +46,14 @@ class DescriptionField extends Component {
 }
 
 DescriptionField.defaultProps = {
-  isPrivate: false
+  isPrivate: false,
+  onBlurDescription: () => {}
 }
 
 DescriptionField.propTypes = {
   challenge: PropTypes.shape().isRequired,
   onUpdateDescription: PropTypes.func.isRequired,
+  onBlurDescription: PropTypes.func,
   type: PropTypes.string.isRequired,
   isPrivate: PropTypes.bool
 }
