70bed47ee1ef0cfa791c68a1492c0b772a5f8e71
diazz <dat.nguyen.supper@gmail.com>
2020-05-06 10:39:27 +0700

    #245 Make `Community` review the default
    #245 Make `Community` review the default

--- src/components/ChallengeEditor/index.js
+++ src/components/ChallengeEditor/index.js
@@ -490,7 +490,7 @@ class ChallengeEditor extends Component {
       startDate: moment().add(1, 'days').format(),
       legacy: {
         track,
-        reviewType: 'INTERNAL'
+        reviewType: 'community'
       }
     }
     try {
