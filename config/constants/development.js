f60d97e29b6e23817acd9e9421e2abfd11dcd158
diazz <dat.nguyen.supper@gmail.com>
2020-05-10 11:03:26 +0700

    #564 Link to Direct & OR
    #564 Link to Direct & OR

--- config/constants/development.js
+++ config/constants/development.js
@@ -1,12 +1,13 @@
-const DEV_API_HOSTNAME = 'https://api.topcoder-dev.com'
+const DOMAIN = 'topcoder-dev.com'
+const DEV_API_HOSTNAME = `https://api.${DOMAIN}`
 
 module.exports = {
-  ACCOUNTS_APP_CONNECTOR_URL: 'https://accounts.topcoder-dev.com/connector.html',
-  ACCOUNTS_APP_LOGIN_URL: 'https://accounts.topcoder-dev.com/member',
-  COMMUNITY_APP_URL: 'https://www.topcoder-dev.com',
+  ACCOUNTS_APP_CONNECTOR_URL: `https://accounts.${DOMAIN}/connector.html`,
+  ACCOUNTS_APP_LOGIN_URL: `https://accounts.${DOMAIN}/member`,
+  COMMUNITY_APP_URL: `https://www.${DOMAIN}`,
   MEMBER_API_URL: `${DEV_API_HOSTNAME}/v4/members`,
   MEMBER_API_V3_URL: `${DEV_API_HOSTNAME}/v3/members`,
-  DEV_APP_URL: 'http://local.topcoder-dev.com',
+  DEV_APP_URL: `http://local.${DOMAIN}`,
   CHALLENGE_API_URL: `${DEV_API_HOSTNAME}/v5/challenges`,
   CHALLENGE_TIMELINE_TEMPLATES_URL: `${DEV_API_HOSTNAME}/v5/timeline-templates`,
   CHALLENGE_TYPES_URL: `${DEV_API_HOSTNAME}/v5/challenge-types`,
@@ -18,5 +19,7 @@ module.exports = {
   RESOURCE_ROLES_API_URL: `${DEV_API_HOSTNAME}/v5/resource-roles`,
   PLATFORMS_V4_API_URL: `${DEV_API_HOSTNAME}/v4/platforms`,
   TECHNOLOGIES_V4_API_URL: `${DEV_API_HOSTNAME}/v4/technologies`,
-  CONNECT_APP_URL: 'https://connect.topcoder-dev.com'
+  CONNECT_APP_URL: `https://connect.${DOMAIN}`,
+  DIRECT_PROJECT_URL: `https://www.${DOMAIN}/direct`,
+  ONLINE_REVIEW_URL: `https://software.${DOMAIN}`
 }
