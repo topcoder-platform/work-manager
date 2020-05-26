d3da36466a8a7dd3020090b63ffc8a4130e9957a
James Cori <jamescori@gmail.com>
2020-05-26 18:36:41 -0400

    Removing https check

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const healthCheck = require('topcoder-healthcheck-dropin')
 
 const app = express()
 
-const requireHTTPS = (req, res, next) => {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
-    return res.redirect('https://' + req.get('host') + req.url)
-  }
-  next()
-}
+// const requireHTTPS = (req, res, next) => {
+//   // The 'x-forwarded-proto' check is for Heroku
+//   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
+//     return res.redirect('https://' + req.get('host') + req.url)
+//   }
+//   next()
+// }
 
 function check () {
   return true
 }
 app.use(healthCheck.middleware([check]))
-app.use(requireHTTPS)
+// app.use(requireHTTPS) // removed because app servers don't handle https
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
