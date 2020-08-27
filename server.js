492e2fd7153e71d2eceee226966586e36359726a
Vikas Agarwal <vikas.agarwal@topcoder.com>
2020-08-27 18:24:55 +0530

    Trying facade for services

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require('express')
 const path = require('path')
 const healthCheck = require('topcoder-healthcheck-dropin')
+const { fetchTimelineTemplates, fetchChallengePhases, fetchChallengeTypes, fetchChallengeTracks, fetchChallengeTimelines, fetchChallengeTags, fetchGroups, fetchResourceRoles } = require('./src/services/challenges')
 
 const app = express()
-
-// const requireHTTPS = (req, res, next) => {
-//   // The 'x-forwarded-proto' check is for Heroku
-//   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== 'development') {
-//     return res.redirect('https://' + req.get('host') + req.url)
-//   }
-//   next()
-// }
 
 function check () {
   return true
 }
 app.use(healthCheck.middleware([check]))
+
+// api facade
+app.get('/api/metadata', async (req, res, next) => {
+  const allMetadataPromises = []
+  allMetadataPromises.push(fetchTimelineTemplates())
+  allMetadataPromises.push(fetchChallengePhases())
+  allMetadataPromises.push(fetchChallengeTypes())
+  allMetadataPromises.push(fetchChallengeTracks())
+  allMetadataPromises.push(fetchChallengeTimelines())
+  allMetadataPromises.push(fetchChallengeTags())
+  allMetadataPromises.push(fetchGroups())
+  allMetadataPromises.push(fetchResourceRoles())
+  const responses = await Promise.all(allMetadataPromises)
+  res.json(responses)
+  next()
+})
+
 // app.use(requireHTTPS) // removed because app servers don't handle https
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
+
+// static content
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
 const port = process.env.PORT || 3000
 app.listen(port)
