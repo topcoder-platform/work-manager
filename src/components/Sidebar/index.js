6050b2ac7b3502f631e3da5552a2aee9f63023da
Vikas Agarwal <vikas.agarwal@topcoder.com>
2020-12-10 17:00:30 +0530

    fix: git#954-Update "Give Application Feedback" Hyperlink

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -20,7 +20,7 @@ const Sidebar = ({
           Active challenges
         </div>
       </Link>
-      <a href='https://forms.gle/CsaVawSDkdR5E92B8' target='_blank' rel='noopener noreferrer' className='chameleon-feedback'>
+      <a href='https://github.com/topcoder-platform/work-manager/issues/new' target='_blank' rel='noopener noreferrer' className='chameleon-feedback'>
         <div className={cn(styles.homeLink, { [styles.active]: !projectId })}>
           Give Application Feedback
         </div>
