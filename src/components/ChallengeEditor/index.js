69cdffa50c3c8d3a4ef579220f8130346ef828fb
Mirza Ilhami <mr.ilhami@gmail.com>
2021-05-17 05:18:31 +0700

    #1143 - Fix pureV5 task NDA required even when not selected

--- src/components/ChallengeEditor/index.js
+++ src/components/ChallengeEditor/index.js
@@ -1040,6 +1040,7 @@ class ChallengeEditor extends Component {
       newChallenge.status = status
       try {
         const challengeId = this.getCurrentChallengeId()
+        const action = await updateChallengeDetails(challengeId, challenge)
         // state can have updated assigned member (in cases where user changes assignments without refreshing the page)
         const { challenge: { copilot, reviewer, type }, assignedMemberDetails: assignedMember } = this.state
         const oldMemberHandle = _.get(oldAssignedMember, 'handle')
@@ -1048,7 +1049,6 @@ class ChallengeEditor extends Component {
         if (assignedMemberHandle !== oldMemberHandle) {
           await this.updateResource(challengeId, 'Submitter', assignedMemberHandle, oldMemberHandle)
         }
-        const action = await updateChallengeDetails(challengeId, challenge)
         const { copilot: previousCopilot, reviewer: previousReviewer } = this.state.draftChallenge.data
         if (copilot !== previousCopilot) await this.updateResource(challengeId, 'Copilot', copilot, previousCopilot)
         if (type === 'First2Finish' || type === 'Task') {
