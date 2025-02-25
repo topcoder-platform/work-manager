c41bf69343262e52a8d74125448519bdcdefb1c3
Hentry Martin <hentrymartin.p@gmail.com>
2025-02-25 19:52:03 +0100

    feat: artifact download implementation

--- src/components/ChallengeEditor/ArtifactsListModal/index.js
+++ src/components/ChallengeEditor/ArtifactsListModal/index.js
@@ -4,7 +4,7 @@ import Modal from '../../Modal'
 import styles from './ArtifactsListModal.module.scss'
 import PropTypes from 'prop-types'
 import ReactSVG from 'react-svg'
-import { getTopcoderReactLib } from '../../../util/topcoder-react-lib'
+import { getTopcoderReactLib, isValidDownloadFile } from '../../../util/topcoder-react-lib'
 import Loader from '../../Loader'
 const assets = require.context('../../../assets/images', false, /svg/)
 
@@ -19,6 +19,17 @@ export const ArtifactsListModal = ({ onClose, submissionId, token, theme }) => {
     const { artifacts: resp } = await submissionsService.getSubmissionArtifacts(submissionId)
     setArtifacts(resp)
     setLoading(false)
+  }
+
+  const getExtensionFromMime = (mimeType) => {
+    const mimeMap = {
+      'application/zip': 'zip',
+      'application/pdf': 'pdf',
+      'image/jpeg': 'jpg',
+      'image/png': 'png',
+      'text/plain': 'txt'
+    }
+    return mimeMap[mimeType] || 'zip'
   }
 
   useEffect(() => {
@@ -39,7 +50,36 @@ export const ArtifactsListModal = ({ onClose, submissionId, token, theme }) => {
               return (
                 <div className={styles['list-item']}>
                   <div className={styles['artifact-name']}>{item}</div>
-                  <ReactSVG className={styles['icon-download']} path={assets('./IconSquareDownload.svg')} />
+                  <ReactSVG
+                    className={styles['icon-download']}
+                    path={assets('./IconSquareDownload.svg')}
+                    onClick={() => {
+                      // download submission
+                      const reactLib = getTopcoderReactLib()
+                      const { getService } = reactLib.services.submissions
+                      const submissionsService = getService(token)
+                      submissionsService.downloadSubmissionArtifact(submissionId, item)
+                        .then((blob) => {
+                          isValidDownloadFile(blob).then((isValidFile) => {
+                            if (isValidFile.success) {
+                              // eslint-disable-next-line no-undef
+                              const blobFile = new Blob([blob])
+                              const url = window.URL.createObjectURL(blobFile)
+                              const link = document.createElement('a')
+                              link.href = url
+                              const extension = getExtensionFromMime(blob.type)
+                              const fileName = `${submissionId}.${extension}`
+                              link.setAttribute('download', `${fileName}`)
+                              document.body.appendChild(link)
+                              link.click()
+                              link.parentNode.removeChild(link)
+                            } else {
+                              console.log(isValidFile, 'failed')
+                            }
+                          })
+                        })
+                    }}
+                  />
                 </div>
               )
             })
