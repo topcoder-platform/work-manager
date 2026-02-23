819d894a2b6cb08b22674886f0614c00992a331a
Vasilica Olariu <olariu.vasilica@gmail.com>
2026-02-23 20:53:38 +0200

    fix api for templates fetching

--- src/services/aiReviewTemplates.js
+++ src/services/aiReviewTemplates.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import qs from 'qs'
 
-import { TC_REVIEWS_API_BASE_URL } from '../config/constants'
+import { API_V6 } from '../config/constants'
 
 import { axiosInstance } from './axiosWithAuth'
 
@@ -15,7 +15,7 @@ export async function fetchAIReviewTemplates (filters = {}) {
     const queryString = Object.keys(filters).length > 0
       ? `?${qs.stringify(filters, { encode: false })}`
       : ''
-    const response = await axiosInstance.get(`${TC_REVIEWS_API_BASE_URL}/ai-review/templates${queryString}`)
+    const response = await axiosInstance.get(`${API_V6}/ai-review/templates${queryString}`)
     return _.get(response, 'data', [])
   } catch (error) {
     console.error('Error fetching AI review templates:', error.message)
